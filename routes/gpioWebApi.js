e2d290c1b883d694ce46a05b8b5de53484313588
Jarek Kuliga <j.kuliga.kontakt@gmail.com>
2016-11-29 20:46:01 +0100

    web api for test led

--- routes/gpioWebApi.js
+++ routes/gpioWebApi.js
@@ -9,19 +9,29 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/status/:id', function(req, res, next) {
-  res.render('index', { title: 'Status by id' });
+  res.render('index', { title: req.params.id+':'+(+!wpi.digitalRead(parseInt(req.params.id))) });
 });
 
 router.get('/on/:id', function(req, res, next) {
-  res.render('index', { title: 'On led by id' });
+  const pin = parseInt(req.params.id);
+  wpi.pinMode(pin, wpi.OUTPUT);
+  wpi.digitalWrite(pin, 0);
+  res.render('index', { title: 'On led: '+pin });
 });
 
 router.get('/off', function(req, res, next) {
+  for (var i = 1; i < 4; i++) {
+    wpi.pinMode(i, wpi.OUTPUT);
+    wpi.digitalWrite(i, 1);
+  }
   res.render('index', { title: 'Off all' });
 });
 
 router.get('/off/:id', function(req, res, next) {
-  res.render('index', { title: 'Off led by id' });
+  const pin = parseInt(req.params.id);
+  wpi.pinMode(pin, wpi.OUTPUT);
+  wpi.digitalWrite(pin, 1);
+  res.render('index', { title: 'Off led: '+pin });
 });
 
 module.exports = router;
